9b661ff7ccb5c2f9fbcaa6c7a13ac4cf0bf2dbca
Pedro Júnior <pedro.eustaquio>
2021-01-03 12:36:17 -0300

    Dashboard

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:bytebank_v2/dashboard.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BytebankApp());
+}
+
+class BytebankApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Dashboard(),
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        accentColor: Colors.blueAccent[700],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent[700],
+          textTheme: ButtonTextTheme.primary,
+        )
+      ),
+    );
+  }
+}
